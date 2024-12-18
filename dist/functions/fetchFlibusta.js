b70f0ffa09358686bf08521bd6453ebc835071fc
gineff <canone@inbox.ru>
2024-12-18 14:50:36 +0300

    fix: fix flibusta fetch

--- dist/functions/fetchFlibusta.js
+++ dist/functions/fetchFlibusta.js
@@ -1,31 +1,46 @@
-//test
 exports = async () => {
   const { getText, xmlParser } = context.functions.execute("mainFunctions");
   const Books = context.services.get("mongodb-atlas").db("flibusta").collection("Books");
 
-  const getUrl = (page) => `https://flibusta.is/opds/new/${page}/new`;
-
   async function* fetchBooks() {
-    let page = 0;
-    let url = getUrl(page);
+    const getUrl = (page) => `https://flibusta.is/opds/new/${page}/new`;
 
-    while (url) {
-      const xml = await getText(fetchUrl);
+    let page = 0;
+
+    while (true) {
+      const url = getUrl(page);
+      const xml = await getText(url);
+
+      if (!xml) {
+        break;
+      }
+
       const json = xmlParser(xml);
+
+      if (!json || json.length === 0) {
+        break;
+      }
+
       for (const book of json) {
         yield book;
       }
 
       page += 1;
-      url = getUrl(page);
     }
   }
 
   for await (const book of fetchBooks()) {
     try {
-      Books.insert(book);
+      const existingBook = await Books.findOne({ bid: book.bid });
+
+      if (!existingBook) {
+        await Books.insertOne(book);
+        console.log(`Добавлена книга: ${book.title}`);
+      } else {
+        break;
+      }
     } catch (error) {
-      console.error(error);
+      console.error(`Ошибка при обработке книги "${book.title}":`, error);
       break;
     }
   }
