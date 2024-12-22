d52f609f2d4c71e48035435db857c870282c95d4
gineff <canone@inbox.ru>
2024-12-22 17:45:54 +0300

    fix: fix pipe call

--- dist/functions/uploadBooksToS3.js
+++ dist/functions/uploadBooksToS3.js
@@ -5,7 +5,6 @@ const upload = (s3, params) => {
 };
 
 exports = async function uploadBooks(changeEvent) {
-  const Books = context.services.get("mongodb-atlas").db("flibusta").collection("Books");
   const { fullDocument, operationType, updateDescription = {} } = changeEvent;
   const { _id, image, downloads } = fullDocument;
 
@@ -33,29 +32,23 @@ exports = async function uploadBooks(changeEvent) {
     if (image) {
       upload(
         clientS3,
-        await new ImageStreamParams(
-          Object.assign(options, { height: 167, url: image }).pipe().values
-        )
+        await new ImageStreamParams(Object.assign(options, { height: 167, url: image })).pipe()
+          .values
       );
       upload(
         clientS3,
-        await new ImageStreamParams(
-          Object.assign(options, { height: 700, url: image }).pipe().values
-        )
+        await new ImageStreamParams(Object.assign(options, { height: 700, url: image })).pipe()
+          .values
       );
     }
 
-    let fb2AttachmentUrl, fb2FileName;
+    let fb2AttachmentUrl;
 
-    if (fb2AttachmentUrl = getFb2Url(downloads)) {
-      const fb2 = await new FileStreamParams(
-        Object.assign(options, { url: fb2AttachmentUrl }).pipe().values
+    if ((fb2AttachmentUrl = getFb2Url(downloads))) {
+      upload(
+        clientS3,
+        await new FileStreamParams(Object.assign(options, { url: fb2AttachmentUrl })).pipe().values
       );
-
-      fb2FileName = fb2.fileName;
-      upload(clientS3, fb2.values);
     }
-
-    Books.updateOne({ _id }, { $set: { s3: true, ...(fb2FileName ? { fb2FileName } : {}) } });
   }
 };
